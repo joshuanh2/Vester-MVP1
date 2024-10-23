1e3b9ae03571cad204de07cf871c67793255666d
joshuanh2 <joshuanh20@gmail.com>
2024-10-23 18:57:55 -0400

    again why not

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: {},  // Load Tailwind CSS
+    autoprefixer: {}, // For cross-browser compatibility
   },
 };
 
