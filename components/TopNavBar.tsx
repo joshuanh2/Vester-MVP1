797e77935fb8dd6727a8a53fadfa4410380d4316
joshuanh2 <joshuanh20@gmail.com>
2024-10-24 12:23:36 -0400

    beta signup 1

--- components/TopNavBar.tsx
+++ components/TopNavBar.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Band from '@/components/beta-signup/src/Components/Band'; // Imported Band component
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -20,21 +21,35 @@ interface TopNavBarProps {
 const TopNavBar: React.FC<TopNavBarProps> = ({ features = {} }) => {
   return (
     <div className="flex items-center justify-between p-4">
-      <div className="font-bold text-xl flex gap-2 items-center">
+      <div className="font-bold text-xl flex gap-4 items-center">
+        {/* Beta Signup Button */}
+        <a
+          href="/beta-signup"
+          className="bg-[#129de8] text-white font-semibold py-2 px-4 rounded hover:bg-blue-600"
+        >
+          Beta Signup
+        </a>
+
+        {/* Company Wordmark */}
         <Image
           src="/vester-name.png"
           alt="Company Wordmark"
           width={168}
           height={30}
         />
+        
+        {/* Subtext */}
         <span className="font-bold text-[95%]">Conversational Analytics Proof of Concept</span>
+        
+        {/* Band Component */}
+        <Band />
       </div>
+
       <div className="flex items-center gap-2">
         <a href="mailto:alex@vesterai.com" className="block">
           <img src="/vester-profile.png" alt="Vester Profile" className="w-12 h-12" />
         </a>
       </div>
-
     </div>
   );
 };
