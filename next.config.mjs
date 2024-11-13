fe9899444ec85fb4f31f166d0526c962458ff5cd
joshuanh2 <joshuanh20@gmail.com>
2024-11-13 13:10:27 -0500

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: '/poc',
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
